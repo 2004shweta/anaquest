c6cf4fe43d4b7af6f8d6be93d87a7be46b15dc02
2004shweta <156895477+2004shweta@users.noreply.github.com>
2025-06-24 11:49:56 +0530

    fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,7 @@ import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       admin?: boolean;
     } & DefaultSession["user"];
   }
